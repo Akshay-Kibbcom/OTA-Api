0429f57fbb5a871a6b88a8a6c0e5e835e7e3eddb
Akshay-Kibbcom <akshay.kashyap@kibbcom.in>
2018-10-23 17:59:06 +0530

    New Code

---
+++ index.js
@@ -0,0 +1,107 @@
+var express = require('express')
+var bodyParser = require("body-parser");
+var path = require('path');
+var router = express.Router();
+var multer = require('multer');
+const fs = require('fs');
+const dir = './firmware';
+var app = express()
+
+app.use(bodyParser.json());
+
+
+var Storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, "./firmware");
+    },
+    filename: function (req, file, callback) {
+        callback(null, file.originalname);
+    }
+});
+
+var upload = multer({ storage: Storage }).array("imgUploader", 3); //Field name and max count
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/upload", function (req, res) {
+    upload(req, res, function (err) {
+        if (err) {
+            return res.end("Something went wrong!");
+        }
+        return res.end("File uploaded sucessfully!.");
+    });
+});
+
+// Check File Exist or Not
+function getFileExists(filename)
+{
+    console.log("Inside File Exist Function")
+    var promise= new Promise(function(resolve,reject){
+        fs.readdir(dir, (err, files) => {
+            files.forEach(file => {
+                var fname= file.slice(0, -4);
+                if(fname==filename)
+                {
+                    console.log("File Exist")
+                    return resolve(1);
+                }
+            })
+            return reject(0)
+        })
+    });
+    return promise;
+}
+
+// Get New Version is available of not.
+app.post('/getVersionUpdates', function (req, res) {
+    var body=req.body;
+    var filename= body.filename;
+    console.log("File in URL Body is : "+filename)
+
+    var lastChar = filename[filename.length-1];
+    var ver= parseInt(lastChar)
+    ver++;
+    console.log(ver);
+    var newFile=filename.replace(lastChar,ver);
+    console.log(newFile);
+    console.log(path.resolve("./firmware/"+newFile+".txt"));
+
+
+    var ret=getFileExists(newFile);
+
+    var fileExist;
+
+    ret.then(function(result){
+        console.log("Result: "+result)
+        var response={
+            Result : "New Updates Available",
+            URL : path.resolve("./firmware/"+newFile+".txt")
+        }
+        res.send(response);
+    },function(err)
+    {
+        console.log(err)
+        var response ={
+            Result : "No Updates Available, You are upto date"
+        }
+        res.send(response);
+    })
+})
+
+
+// Download a new Firmware
+app.get('/download/:file(*)',(req, res) => {
+    var file = req.params.file;
+    var fileLocation = path.resolve("./firmware/"+file);
+    console.log(fileLocation);
+    var response= {
+        url : fileLocation
+    }
+    res.send(response) ;
+  });
+
+app.listen(3000, function () {
+    console.log('Magic is happening on port 3000!')
+})
