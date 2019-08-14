051e0937c9fdccae197e01a5e592c99bf1f2cd10
Akshay-Kibbcom <akshay.kashyap@kibbcom.in>
2019-08-14 15:11:12 +0530

    OTA Camera Included

--- index.js
+++ index.js
@@ -8,22 +8,35 @@ var crypto = require('crypto');
 var querystring = require('querystring');
 var http = require('http');
 const dir = './firmware';
+const camDir = './camfirmware';
 var app = express()
 
 app.use(bodyParser.json());
-var checkSum ;
-
-
+ 
+// Fido Firmware Directory
 var Storage = multer.diskStorage({
     destination: function (req, file, callback) {
-        callback(null, "./firmware");
+        callback(null, dir);
     },
     filename: function (req, file, callback) {
         callback(null, file.originalname);
     }
 });
 
+// Cam Firmware Directory
+var camStorage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, camDir);
+    },
+    filename: function (req, file, callback) {
+        callback(null, file.originalname);
+    }
+});
+
+
 var upload = multer({ storage: Storage }).array("imgUploader", 3); //Field name and max count
+
+var camUpload = multer({ storage: camStorage }).array("camimgUploader", 3);
 
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html");
@@ -77,8 +90,6 @@ function sendNotification()
     // post the data
     post_req.write(post_data.toString());
     post_req.end();
-
-    
 }
 
 app.post("/upload", function (req, res) {
@@ -90,6 +101,19 @@ app.post("/upload", function (req, res) {
     });
     sendNotification();
 });
+
+app.post("/uploadCam", function (req, res) {
+    console.log("Upload Cam");
+    camUpload(req, res, function (err) {
+        if (err) {
+            return res.end("Something went wrong!");
+        }
+        return res.end("File uploaded sucessfully!.");
+    });
+    //sendNotification();
+});
+
+
 
 // Check File Exist or Not
 function getFileExists(filename)
@@ -124,75 +148,193 @@ function generateChecksum(str, algorithm, encoding) {
         console.log("Check : "+checksumReturn)
 }
 // Get Maximum Number
-function getMaxVer()
+async function getMaxVer(path)
 {
-    var promise= new Promise(function(resolve,reject){
-        fs.readdir(dir, (err, files) => {
+    //var promise= new Promise(function(resolve,reject){
+        files = fs.readdirSync(path)
+            fileVersions = [];
             files.forEach(file => {
                 console.log(file)
                 var fname= file.slice(0, -4);
                 var ver= fname.split("_")
+                //console.log("Ver ",ver)
                 ver=ver[1];
                 fileVersions.push(ver);
                 
             })
-            return resolve(Math.max(...fileVersions))
-        })
-    });
-    return promise;
+            //console.log("Vers ", fileVersions )
+            //return resolve(Math.max(...fileVersions))
+        //})
+        return Math.max(...fileVersions)
+    //});
+    //return promise;
 }
 
 // Get New Version is available of not.
-app.post('/getVersionUpdates', function (req, res) {
+app.post('/getVersionUpdates', async function (req, res) {
+
+    var fidoCheckSum ;
+    var fidoFirmURL ; 
+    var fidoFirmSize ;
+    var fidoUpdate ;
+
+
     var body=req.body;
     var filename= body.currentVersion;
-    console.log("File in URL Body is : "+filename)
+    var camVersion = body.currentCamVersion;
+    console.log("Fido Version in URL Body is : "+filename)
+    console.log("Cam Version in URL Body is : "+camVersion)
 
     /*// Truncate the Extn
     var filname= filename.slice(0,-4);
     console.log("File without EXTN "+filname);
     */
-
+    //Fido Firmware Split
     var lastChar = filename.split("_")
     lastChar=lastChar[1];
     console.log(lastChar);
     var ver= parseInt(lastChar)
     //ver++;
-    var max=getMaxVer();
-    max.then(function(maxValue){
-        if(maxValue>ver)
-        {
-            var newFile;
-            fs.readdir(dir, (err, files) => {
-                files.forEach(file => {
-                    if(file.indexOf(maxValue) == -1 ?false:true)
-                    {
-                        console.log("Update file name : "+file);
-                        newFile=file;
-                        console.log("./firmware/"+newFile)
-                        // Get File Size
-                        const stats = fs.statSync("./firmware/"+newFile)
-                        const fileSizeInBytes = stats.size
-                        //Convert the file size to megabytes (optional)
-                        //const fileSizeInMegabytes = fileSizeInBytes / 1000000.0
+    var max= await getMaxVer(dir);
+    console.log("Fido Max", max)
 
-                        fs.readFile('./firmware/'+newFile, function(err, data) {
-                        console.log(data)
-                        generateChecksum(data); 
-                        console.log(checksumReturn)  
+    // Cam Firmware Split
+    var camlastChar = camVersion.split("_")
+    camlastChar=camlastChar[1];
+    console.log(camlastChar);
+    var Camver= parseInt(camlastChar)
+
+    var camMax = await getMaxVer(camDir);
+
+    console.log("Cam Version", Camver)
+    console.log("Cam Max", camMax)
+
+    // Fido Firmware File URL
+    //var res = await max.then(result => result.data)
+    // var res = await max.then(function(maxValue){
+    if(max>ver)
+    {
+        var newFile;
+        files = fs.readdirSync(dir)
+            //, (err, files) => {
+            files.forEach(file => {
+                if(file.indexOf(max) == -1 ?false:true)
+                {
+                    console.log("Update file name : "+file);
+                    newFile=file;
+                    console.log("./firmware/"+newFile)
+                    // Get File Size
+                    const stats = fs.statSync("./firmware/"+newFile)
+                    const fileSizeInBytes = stats.size
+                    //Convert the file size to megabytes (optional)
+                    //const fileSizeInMegabytes = fileSizeInBytes / 1000000.0
+
+                    data = fs.readFileSync('./firmware/'+newFile)
+                    console.log("Data ", data)
+                    // , function(err, data) {
+                    // console.log(data)
+                    generateChecksum(data); 
+                    console.log(checksumReturn)  
+                    // var response={
+                    //         Confirmed : 0,
+                    //         Result : "New Updates Available",
+                    //         URL : "http://159.65.152.85:4000/download/"+newFile,
+                    //         Checksum : checksumReturn,
+                    //         Filesize : fileSizeInBytes
+                    //     }
+                    //     res.send(response);
+                    fidoUpdate = true; 
+                    fidoFirmURL = "http://159.65.152.85:4000/download/"+newFile;
+                    fidoCheckSum = checksumReturn;
+                    fidoFirmSize = fileSizeInBytes;
+                    //});
+                }
+            //})
+
+        })
+    }
+
+    data = '';
+    checksumReturn = ''
+    fileSizeInBytes = ''
+    newFile = ''
+
+    // })
+    //console.log("Res ", res)
+
+    console.log("Fido Update", fidoUpdate)
+    console.log("Fido URL", fidoFirmURL)
+    console.log("Fido Checksum", fidoCheckSum)
+    console.log("Fido FileSize", fidoFirmSize)
+    // Camera Firmware File URL
+
+    // camMax.then(function(maxVersion){
+    //     console.log("Cam Max", maxVersion)
+    if(camMax>Camver)
+    {
+        var newFile;
+        fs.readdir(camDir, (err, files) => {
+            files.forEach(file => {
+                if(file.indexOf(camMax) == -1 ?false:true)
+                {
+                    console.log("Update file name : "+file);
+                    newFile=file;
+                    console.log("./camfirmware/"+newFile)
+                    // Get File Size
+                    const stats = fs.statSync("./camfirmware/"+newFile)
+                    const fileSizeInBytes = stats.size
+                    //Convert the file size to megabytes (optional)
+                    //const fileSizeInMegabytes = fileSizeInBytes / 1000000.0
+
+                    fs.readFile('./camfirmware/'+newFile, function(err, data) {
+                    console.log(data)
+                    generateChecksum(data); 
+                    console.log(checksumReturn)  
+                    if (fidoUpdate){
                         var response={
-                                Confirmed : 0,
-                                Result : "New Updates Available",
-                                URL : "http://159.65.152.85:4000/download/"+newFile,
-                                Checksum : checksumReturn,
-                                Filesize : fileSizeInBytes
-                            }
-                            res.send(response);
-                        });
+                            Confirmed : 0,
+                            Result : "New Updates Available",
+                            fidoFirmware_URL : fidoFirmURL,
+                            fidoFirmware_Checksum : fidoCheckSum,
+                            fidoFirmware_Filesize : fidoFirmSize,
+                            camFirmware_URL : "http://159.65.152.85:4000/download/"+newFile,
+                            camFirmware_Checksum : checksumReturn,
+                            camFirmware_Filesize : fileSizeInBytes,
+                        }
                     }
-                })
-
+                    else{
+                        var response={
+                            Confirmed : 0,
+                            Result : "New Updates Available",
+                            camFirmware_URL : "http://159.65.152.85:4000/download/"+newFile,
+                            camFirmware_Checksum : checksumReturn,
+                            camFirmware_Filesize : fileSizeInBytes,
+                        }
+                    }
+                    // var response={
+                    //         Confirmed : 0,
+                    //         Result : "New Updates Available",
+                    //         URL : "http://159.65.152.85:4000/download/"+newFile,
+                    //         Checksum : checksumReturn,
+                    //         Filesize : fileSizeInBytes
+                    //     }
+                        res.send(response);
+                    });
+                }
             })
+
+        })
+    }
+    else{
+        if(fidoUpdate){
+            var response={
+                Confirmed : 0,
+                Result : "New Updates Available",
+                fidoFirmware_URL : fidoFirmURL,
+                fidoFirmware_Checksum : fidoCheckSum,
+                fidoFirmware_Filesize : fidoFirmSize,
+            }
+            res.send(response);
         }
         else{
             var response ={
@@ -200,41 +342,9 @@ app.post('/getVersionUpdates', function (req, res) {
                 Result : "No Updates Available, You are upto date"
             }
             res.send(response);
-        }
-    })
-    /*console.log(ver);
-    var newFile=filename.replace(lastChar,ver);
-    console.log(newFile);
-    console.log(path.resolve("./firmware/"+newFile+".txt"));
-
-
-    var ret=getFileExists(newFile);
-
-    var fileExist;
-    
-    ret.then(function(result){
-        console.log("Result: "+result)
-        console.log('./firmware/'+newFile+".txt");
-        
-        fs.readFile('./firmware/'+newFile+".txt", function(err, data) {
-            console.log(data)
-            generateChecksum(data); 
-            console.log(checksumReturn)  
-            var response={
-                Result : "New Updates Available",
-                URL : "http://159.65.152.85:4000/download/"+newFile+".txt",
-                Checksum : checksumReturn
-            }
-            res.send(response);
-        });
-    },function(err)
-    {
-        console.log(err)
-        var response ={
-            Result : "No Updates Available, You are upto date"
-        }
-        res.send(response);
-    })*/
+        }     
+    }
+    // })
 })
 
 
@@ -256,5 +366,5 @@ app.get('/download/:file(*)',(req, res) => {
 
 
 app.listen(4000, function () {
-    console.log('Magic is happening on port 3000!')
+    console.log('Magic is happening on port 4000!')
 })
